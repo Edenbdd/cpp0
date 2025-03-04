a438b8aedd31cd0bb377c76cff5afcbca7778c18
Edenbdd <aubertra@student.42.fr>
2025-03-04 09:45:59 +0100

    basic input cmd

---
+++ ex01/includes/phonebook.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aubertra <aubertra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 09:18:39 by aubertra          #+#    #+#             */
+/*   Updated: 2025/03/04 09:41:55 by aubertra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#endif
